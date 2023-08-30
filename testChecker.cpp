3c4a9660d7c6913e6a0a28fb0be793a29e27ac81
Kumar <Vikas.Kumar@philips.com>
2023-08-31 01:47:41 +0530

    Monitor7

--- testChecker.cpp
+++ testChecker.cpp
@@ -3,22 +3,22 @@
 
 using namespace std;
 
-void TestVitals() {
+void TestVitalsMonitor::TestVitals() {
   assert(!VitalsMonitor::vitalsOk(99, 102, 70));
   assert(VitalsMonitor::vitalsOk(98.1, 70, 98));
 }
 
-void TestBodyTemparature() {
+void TestVitalsMonitor::TestBodyTemparature() {
   assert(!VitalsMonitor::bodyTemperatureOk(99));
   assert(VitalsMonitor::bodyTemperatureOk(98.1));
 }
 
-void TestPulseRate() {
+void TestVitalsMonitor::TestPulseRate() {
   assert(!VitalsMonitor::pulseRateOk(102));
   assert(VitalsMonitor::pulseRateOk(70));
 }
 
-void TestOxygenSaturation() {
+void TestVitalsMonitor::TestOxygenSaturation() {
   assert(!VitalsMonitor::oxygenSaturationOk(70));
   assert(VitalsMonitor::oxygenSaturationOk(98));
 }
