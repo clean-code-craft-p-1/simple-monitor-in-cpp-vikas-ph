b752664d6c8dd007076b73c048f7119720e3d957
Kumar <Vikas.Kumar@philips.com>
2023-08-31 01:27:32 +0530

    Monitor2

--- checker.cpp
+++ checker.cpp
@@ -1,23 +1,18 @@
 #include "checker.h"
 
 using namespace std;
-using namespace VitalsChecker;
 
-int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if(0 == bodyTemperatureOk(temperature)) {
-    return 0;
-  }
-  else if (0 == pulseRateOk(pulseRate)) {
-    retrun 0;
-  }
-  else if (0 == oxygenSaturationOk(spo2)) {
+int TestVitalChecker::vitalsOk(float temperature, float pulseRate, float spo2) {
+  if (0 == TestVitalChecker::bodyTemperatureOk(temperature)) ||
+     (0 == TestVitalChecker::pulseRateOk(pulseRate))         ||
+     (0 == TestVitalChecker::oxygenSaturationOk(spo2)) {
     return 0;
   }
 
   return 1;
 }
 
-int bodyTemperatureOk(float temperature) {
+int TestVitalChecker::bodyTemperatureOk(float temperature) {
   if(temperature > 102 || temperature < 95) {
       cout << "Temperature critical!\n";
       for (int i = 0; i < 6; i++)
@@ -32,7 +27,7 @@ int bodyTemperatureOk(float temperature) {
     return 1;
 }
 
-int pulseRateOk(float pulseRate) {
+int TestVitalChecker::pulseRateOk(float pulseRate) {
   if(pulseRate < 60 || pulseRate > 100) {
       cout << "Pulse Rate is out of range!\n";
       for (int i = 0; i < 6; i++)
@@ -47,7 +42,7 @@ int pulseRateOk(float pulseRate) {
     return 1;
 }
 
-int oxygenSaturationOk(float spo2) {
+int TestVitalChecker::oxygenSaturationOk(float spo2) {
   if(spo2 < 90) {
       cout << "Oxygen Saturation out of range!\n";
       for (int i = 0; i < 6; i++)
--- testChecker.cpp
+++ testChecker.cpp
@@ -1,33 +1,32 @@
 #include "testChecker.h"
 #include "checker.h"
 
-using namespace VitalsChecker;
-using namespace TestVitalChecker;
+using namespace std;
 
 void TestVitals() {
-  assert(!vitalsOk(99, 102, 70));
-  assert(vitalsOk(98.1, 70, 98));
+  assert(!VitalsChecker::vitalsOk(99, 102, 70));
+  assert(VitalsChecker::vitalsOk(98.1, 70, 98));
 }
 
 void TestBodyTemparature() {
-  assert(!temperatureOk(99));
-  assert(temperatureOk(98.1));
+  assert(!VitalsChecker::temperatureOk(99));
+  assert(VitalsChecker::temperatureOk(98.1));
 }
 
 void TestPulseRate() {
-  assert(!pulseRateOk(102));
-  assert(pulseRateOk(70));
+  assert(!VitalsChecker::pulseRateOk(102));
+  assert(VitalsChecker::pulseRateOk(70));
 }
 
 void TestOxygenSaturation() {
-  assert(!oxygenSaturationOk(70));
-  assert(oxygenSaturationOk(98));
+  assert(!VitalsChecker::oxygenSaturationOk(70));
+  assert(VitalsChecker::oxygenSaturationOk(98));
 }
 
 int main() {
-  TestVitals();
-  TestBodyTemparature();
-  TestPulseRate();
-  TestOxygenSaturation();
+  TestVitalChecker::TestVitals();
+  TestVitalChecker::TestBodyTemparature();
+  TestVitalChecker::TestPulseRate();
+  TestVitalChecker::TestOxygenSaturation();
   cout << "Done\n";
 }
