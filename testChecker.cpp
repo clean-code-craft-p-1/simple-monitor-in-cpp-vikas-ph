900bb84f580db5909f8815149bf42af543bb42d8
Kumar <Vikas.Kumar@philips.com>
2023-08-31 16:38:25 +0530

    Monitor17

--- testChecker.cpp
+++ testChecker.cpp
@@ -9,15 +9,15 @@ void TestVitalsMonitor::TestVitals() {
 void TestVitalsMonitor::TestBodyTemparature() {
   assert(!VitalsMonitor::bodyTemperatureOk(99));
   assert(VitalsMonitor::bodyTemperatureOk(98.1));
-  assert(!VitalsMonitor::isBodyTemperatureNotOk(99));
-  assert(!VitalsMonitor::isBodyTemperatureNotOk(98.1));
+  assert(false == VitalsMonitor::isBodyTemperatureNotOk(99));
+  assert(false == VitalsMonitor::isBodyTemperatureNotOk(98.1));
 }
 
 void TestVitalsMonitor::TestPulseRate() {
   assert(!VitalsMonitor::pulseRateOk(102));
   assert(VitalsMonitor::pulseRateOk(70));
-  assert(VitalsMonitor::isPulseRateNotOk(102));
-  assert(!VitalsMonitor::isPulseRateNotOk(70));
+  assert(true == VitalsMonitor::isPulseRateNotOk(102));
+  assert(false == VitalsMonitor::isPulseRateNotOk(70));
 }
 
 void TestVitalsMonitor::TestOxygenSaturation() {
