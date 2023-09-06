abf7691ca507bb2484c755d6f6f08eca1bee4b31
Kumar <Vikas.Kumar@philips.com>
2023-09-06 20:45:11 +0530

    comment10

--- testChecker.cpp
+++ testChecker.cpp
@@ -2,23 +2,24 @@
 #include "checker.h"
 
 void TestVitalsMonitor::TestVitals() {
-    assert(!VitalsMonitor::vitalsOk(99, 102, 70));
-    assert(VitalsMonitor::vitalsOk(98.1, 70, 98));
+    assert(!VitalsMonitor::vitalsOk(99,  102, 70));
+    assert(!VitalsMonitor::vitalsOk(98.1, 70, 98));
+    assert( VitalsMonitor::vitalsOk(98.1, 70, 88));
 }
 
 void TestVitalsMonitor::TestBodyTemparature() {
-    assert(VitalsMonitor::bodyTemperatureOk(99));
-    assert(VitalsMonitor::bodyTemperatureOk(98.1));
+    assert(!VitalsMonitor::bodyTemperatureOk(102));
+    assert( VitalsMonitor::bodyTemperatureOk(98.1));
 }
 
 void TestVitalsMonitor::TestPulseRate() {
     assert(!VitalsMonitor::pulseRateOk(102));
-    assert(VitalsMonitor::pulseRateOk(70));
+    assert( VitalsMonitor::pulseRateOk(70));
 }
 
 void TestVitalsMonitor::TestOxygenSaturation() {
     assert(!VitalsMonitor::oxygenSaturationOk(70));
-    assert(VitalsMonitor::oxygenSaturationOk(98));
+    assert( VitalsMonitor::oxygenSaturationOk(98));
 }
 
 void TestVitalsMonitor::TestDataRange() {
